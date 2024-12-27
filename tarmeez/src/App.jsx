b339921dc38193c584fdd1d808fd762ac4695684
shehab <shehab.mm@gmail.com>
2024-12-27 13:28:52 +0200

    first commit

---
+++ tarmeez/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Bankform from './components/Bankform'
+function App() {
+
+  return (
+    <>
+    <Bankform/>
+    </>
+  )
+}
+
+export default App
---
+++ tarmeez/src/components/Bankform.jsx
@@ -0,0 +1,16 @@
+
+export default function Bankform() {
+  return (
+    <div>Account Request
+
+
+<form>
+<label htmlFor="">Name</label>
+<input type="text" />
+
+</form>
+
+
+    </div>
+  )
+}
