22ca8b4f1fdaec0c6c0ffd480822c2abad299a66
shehab <shehab.mm@gmail.com>
2024-12-27 23:08:30 +0200

    form check

--- tarmeez/src/components/Bankform.jsx
+++ tarmeez/src/components/Bankform.jsx
@@ -1,8 +1,32 @@
 import { useState } from "react";
+import Modal from "./Modal";
 
 export default function Bankform() {
-  const [name, setname] = useState("");
-  console.log(name);
+  const [inputs, setinputs] = useState({
+    name: "",
+    show: false,
+    phone: "",
+    age: "",
+    ischecked: false,
+    salary: "",
+  });
+  const [err, seterr]= useState(null)
+
+  const submit = (e) => {
+    e.preventDefault();
+    setinputs({ ...inputs, show: true });
+    console.log(inputs);
+    seterr(null)
+    if (inputs.age <18 || inputs.age>100) {
+        seterr(true)
+    }
+
+  };
+
+  if (inputs.show) {
+    setTimeout(() => {  setinputs({...inputs, show:false}) }, 2000)
+   
+  }
   return (
     <div
       style={{
@@ -14,13 +38,12 @@ export default function Bankform() {
         flexDirection: "column",
         justifyContent: "center",
         color: "white",
-        margin:"auto",
-        backgroundColor:"blue",
-        borderRadius:'5rem'
-
+        margin: "auto",
+        backgroundColor: "blue",
+        borderRadius: "5rem",
       }}
     >
-      <h1 style={{margin:"auto"}}>Account Request</h1>
+      <h1 style={{ margin: "auto" }}>Account Request</h1>
 
       <br />
       <form
@@ -30,30 +53,71 @@ export default function Bankform() {
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
-          border:"5rem",
+          border: "5rem",
         }}
+        onSubmit={submit}
       >
-          <input
-            onChange={(e) => {
-              setname(e.target.value);
-            }}
-            type="text"
-            placeholder="name"
-          />
-          <input type="text" placeholder="Phone" />
-          <input  type="number" placeholder="Age" />
-          <label htmlFor="">Are you a member in our bank</label>
-          <input type="checkbox" />
-          <label htmlFor="">Salary </label>
-          <select>
-<option value="">Less than 500$</option>
-<option value="">between 500$ and 700$</option>
-<option value="">above 2000$</option>
-<button>submit</button>
-
-          </select>
-          <button style={{padding:'0.5rem',maginTop:"0.5rem"}}>Submit</button>
+        <input
+          onChange={(e) => {
+            setinputs({ ...inputs, name: e.target.value });
+          }}
+          type="text"
+          placeholder="name"
+          value={inputs.name}
+        />
+        <input
+          onChange={(e) => {
+            setinputs({ ...inputs, phone: e.target.value });
+          }}
+          type="text"
+          placeholder="Phone"
+          value={inputs.phone}
+        />
+        <input
+          onChange={(e) => {
+            setinputs({ ...inputs, age: e.target.value });
+          }}
+          type="number"
+          value={inputs.age}
+          placeholder="Age"
+        />
+        <label htmlFor="">Are you a member in our bank</label>
+        <input
+          onChange={(e) => {
+            setinputs({ ...inputs, ischecked: e.target.checked });
+          }}
+          checked={inputs.ischecked}
+          type="checkbox"
+        />
+        <label htmlFor="">Salary </label>
+        <select
+          value={inputs.salary}
+          onChange={(e) => {
+            setinputs({ ...inputs, salary: e.target.value });
+          }}
+        >
+          <option>Less than 500$</option>
+          <option>between 500$ and 700$</option>
+          <option>above 2000$</option>
+          <button>submit</button>
+        </select>
+        <>
+          <button
+            style={
+              inputs.name == ""
+                ? { backgroundColor: "grey" }
+                : { backgroundColor: "red" }
+            }
+            disabled={
+              inputs.name == "" || inputs.age == "" || inputs.phone == ""
+            }
+          >
+            Submit
+          </button>
+        </>
       </form>
+
+      {inputs.show ? <Modal  errorrmessage={err} /> : ""}
     </div>
   );
 }
---
+++ tarmeez/src/components/Modal.jsx
@@ -0,0 +1,15 @@
+export default function Modal({errorrmessage}){
+
+
+return (
+
+
+<div>
+
+   {errorrmessage?"age is not allowed":"The Form Has Been Submitted Successfully"}
+</div>
+
+)
+
+
+}
